469130045bf15d6688d2a79c2fe65fc2689f3ac6
Sheikh Mohsin <sheikhmohsin9311@gmail.com>
2026-01-15 00:00:19 +0530

    This is done

--- demo.cpp
+++ demo.cpp
@@ -14,18 +14,27 @@ int main() {
 "%c%s%c;\n"
 "    char buffer[10000];\n"
 "    snprintf(buffer, sizeof(buffer), s, 34, s, 34);\n"
-"    ofstream out(\"demo1.cpp\");\n"
-"    out << buffer;\n"
-"    out.close();\n"
+"\n"
+"    for (int i = 1; i <= 10; i++) {\n"
+"        char filename[20];\n"
+"        snprintf(filename, sizeof(filename), \"demo%d.cpp\", i);\n"
+"        ofstream out(filename);\n"
+"        out << buffer;\n"
+"        out.close();\n"
+"    }\n"
 "    return 0;\n"
 "}\n";
 
     char buffer[10000];
     snprintf(buffer, sizeof(buffer), s, 34, s, 34);
 
-    ofstream out("demo1.cpp");
-    out << buffer;
-    out.close();
+    for (int i = 1; i <= 10; i++) {
+        char filename[20];
+        snprintf(filename, sizeof(filename), "demo%d.cpp", i);
+        ofstream out(filename);
+        out << buffer;
+        out.close();
+    }
 
     return 0;
 }
