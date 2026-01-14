16952ea3bd25b8cb65f3d8cb30109250974a7480
Sheikh Mohsin <sheikhmohsin9311@gmail.com>
2026-01-14 10:43:16 +0530

    Initial commit

---
+++ 01hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const char* s =
+"#include <iostream>\n"
+"using namespace std;\n\n"
+"int main() {\n"
+"    const char* s =\n"
+"%c%s%c;\n"
+"    printf(s, 34, s, 34);\n"
+"    return 0;\n"
+"}\n";
+    printf(s, 34, s, 34);
+    return 0;
+}
