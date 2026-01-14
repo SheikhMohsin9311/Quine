92f05719799ecf84ac9a11d58754e1953cb77d2b
Sheikh Mohsin <sheikhmohsin9311@gmail.com>
2026-01-14 11:14:58 +0530

    Add demo.cpp and remove 01hello.cpp

---
+++ demo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const char* s = "This is something that we want to work on and here goes nothing.";
+    printf(s);
+    fork();
+    return 0;
+}
